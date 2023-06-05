a0ac463563e3c97a40e6b13f2b2698887c2e8f43
aaugustynka <aaugustynka.alicja@gmail.com>
2023-06-05 14:20:06 +0200

    Dodaj pliki projektów.

---
+++ DocumentFiller/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DocumentFiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFillDocument = new System.Windows.Forms.Button();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.cmbDocuments = new System.Windows.Forms.ComboBox();
+            this.pnlFields = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFillDocument
+            // 
+            this.btnFillDocument.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnFillDocument.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnFillDocument.Location = new System.Drawing.Point(279, 224);
+            this.btnFillDocument.Name = "btnFillDocument";
+            this.btnFillDocument.Size = new System.Drawing.Size(218, 64);
+            this.btnFillDocument.TabIndex = 1;
+            this.btnFillDocument.Text = "Uzupełnij dane w dokumencie";
+            this.btnFillDocument.UseVisualStyleBackColor = false;
+            this.btnFillDocument.Click += new System.EventHandler(this.btnFillDocument_Click);
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnSelectFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSelectFile.Location = new System.Drawing.Point(279, 140);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(218, 61);
+            this.btnSelectFile.TabIndex = 2;
+            this.btnSelectFile.Text = "Dodaj dokumnet do edycji";
+            this.btnSelectFile.UseVisualStyleBackColor = false;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // cmbDocuments
+            // 
+            this.cmbDocuments.FormattingEnabled = true;
+            this.cmbDocuments.Location = new System.Drawing.Point(217, 110);
+            this.cmbDocuments.Name = "cmbDocuments";
+            this.cmbDocuments.Size = new System.Drawing.Size(354, 24);
+            this.cmbDocuments.TabIndex = 3;
+            this.cmbDocuments.SelectedIndexChanged += new System.EventHandler(this.cmbDocuments_SelectedIndexChanged);
+            // 
+            // pnlFields
+            // 
+            this.pnlFields.Location = new System.Drawing.Point(12, 12);
+            this.pnlFields.Name = "pnlFields";
+            this.pnlFields.Size = new System.Drawing.Size(57, 53);
+            this.pnlFields.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(163, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Po dodaniu wybierz dokument z listy i kliknij uzupełnij";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbDocuments);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.btnFillDocument);
+            this.Controls.Add(this.pnlFields);
+            this.Name = "Form1";
+            this.Text = "uzupełnij";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnFillDocument;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.ComboBox cmbDocuments;
+        private System.Windows.Forms.Panel pnlFields;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DocumentFiller/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace DocumentFiller
+{
+    public partial class Form1 : Form
+    {
+        private DocumentData documentData;
+        private string selectedFilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFillDocument_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedFilePath))
+            {
+                MessageBox.Show("Wybierz plik dokumentu.");
+                return;
+            }
+
+            if (cmbDocuments.SelectedItem is Document selectedDocument)
+            {
+                string xmlFileName = $"output_{Guid.NewGuid()}.xml";
+                string selectedFilePath = selectedDocument.TemplatePath;
+
+                // Utworzenie dokumentu Word z szablonu
+                using (WordprocessingDocument document = WordprocessingDocument.Open(selectedFilePath, true))
+                {
+                    // Uzupełnienie pól formularza
+                    foreach (System.Windows.Forms.TextBox textBox in pnlFields.Controls.OfType<System.Windows.Forms.TextBox>())
+                    {
+                        FillFormField(document, textBox.Name, textBox.Text);
+                    }
+                }
+
+                // Przechwytywanie zdarzenia zamknięcia głównego okna aplikacji
+                FormClosing += (s, args) =>
+                {
+                   
+                    SaveDataToXml(xmlFileName, documentData);
+                };
+
+                Process.Start(selectedFilePath);
+
+                MessageBox.Show("Dokument został uzupełniony. Dane zostaną zapisane do pliku XML po zamknięciu aplikacji.");
+            }
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Pliki Word (*.docx)|*.docx|Wszystkie pliki (*.*)|*.*";
+            openFileDialog.Title = "Wybierz plik dokumentu";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFilePath = openFileDialog.FileName;
+                string documentName = Path.GetFileNameWithoutExtension(selectedFilePath);
+
+                Document newDocument = new Document(documentName, selectedFilePath);
+                documentData.Documents.Add(newDocument);
+
+                cmbDocuments.DataSource = null;
+                cmbDocuments.DataSource = documentData.Documents;
+                cmbDocuments.DisplayMember = "Name";
+            }
+        }
+
+        private void cmbDocuments_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbDocuments.SelectedItem is Document selectedDocument)
+            {
+                // Usuń istniejące kontrolki TextBox
+                pnlFields.Controls.Clear();
+
+                // Pobierz listę nazw pól formularza z wybranego szablonu
+                List<string> formFieldNames = GetFormFieldNamesFromTemplate(selectedDocument.TemplatePath);
+
+                // Dodaj nowe kontrolki TextBox dla pól formularza
+                foreach (string fieldName in formFieldNames)
+                {
+                    Label label = new Label();
+                    label.Text = fieldName;
+                    TextBox textBox = new TextBox();
+                    textBox.Name = fieldName;
+                    textBox.Dock = DockStyle.Fill;
+
+                    pnlFields.Controls.Add(label);
+                    pnlFields.Controls.Add(textBox);
+                }
+            }
+        }
+
+        private void FillFormField(WordprocessingDocument document, string fieldName, string value)
+        {
+            var tag = document.MainDocumentPart.Document.Body.Descendants<Tag>()
+                .FirstOrDefault(t => t.Val == fieldName);
+
+            if (tag != null)
+            {
+                var sdtElement = tag.Parent;
+                var textElement = sdtElement.Descendants<Text>().FirstOrDefault();
+
+                if (textElement != null)
+                {
+                    textElement.Text = value;
+                }
+            }
+        }
+
+        private List<string> GetFormFieldNamesFromTemplate(string templatePath)
+        {
+            List<string> formFieldNames = new List<string>();
+
+            using (WordprocessingDocument document = WordprocessingDocument.Open(templatePath, false))
+            {
+                foreach (SdtElement element in document.MainDocumentPart.Document.Body.Descendants<SdtElement>())
+                {
+                    if (element.Descendants<Tag>().FirstOrDefault() is Tag tag)
+                    {
+                        string fieldName = tag.Val;
+                        if (!formFieldNames.Contains(fieldName))
+                        {
+                            formFieldNames.Add(fieldName);
+                        }
+                    }
+                }
+            }
+
+            return formFieldNames;
+        }
+
+        private void SaveDataToXml(string xmlFileName, DocumentData data)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(DocumentData));
+            using (TextWriter writer = new StreamWriter(xmlFileName))
+            {
+                serializer.Serialize(writer, data);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            documentData = new DocumentData();
+            cmbDocuments.DataSource = documentData.Documents;
+            cmbDocuments.DisplayMember = "Name";
+        }
+    }
+
+    public class DocumentData
+    {
+        public List<Document> Documents { get; set; }
+
+        public DocumentData()
+        {
+            Documents = new List<Document>();
+        }
+    }
+
+    public class Document
+    {
+        public string Name { get; set; }
+        public string TemplatePath { get; set; }
+
+        public Document()
+        {
+        }
+
+        public Document(string name, string templatePath)
+        {
+            Name = name;
+            TemplatePath = templatePath;
+        }
+    }
+}
+
+
